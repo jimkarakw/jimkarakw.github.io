5401a53d1f4a6d977b35e9fb4f1a9be6a23ceee0
Jim_Karakw <dimitris.karakwttas@gmail.com>
2023-01-10 16:03:19 +0200

    Anchor link Fix

--- time_and_date.js
+++ time_and_date.js
@@ -22,5 +22,28 @@ setInterval(renderTime, 1000);
 
 //End Display date & time
 
+//Anchor links
+window.addEventListener(
+  "DOMContentLoaded",
+  function (e) {
+    var links = document.getElementsByTagName("A");
+    for (var i = 0; i < links.length; i++) {
+      if (!links[i].hash) continue;
+      if (links[i].origin + links[i].pathname != self.location.href) continue;
+      (function (anchorPoint) {
+        links[i].addEventListener(
+          "click",
+          function (e) {
+            anchorPoint.scrollIntoView(true);
+            e.preventDefault();
+          },
+          false
+        );
+      })(document.getElementById(links[i].hash.replace(/#/, "")));
+    }
+  },
+  false
+);
+
 //Auto Year
 document.querySelector("#copyright-year").innerText = new Date().getFullYear();
